99b89e5a8758a05ee5af88db7b2f716b42cac47b
Maxyapper7 <65361371+Maxyapper7@users.noreply.github.com>
2022-08-17 17:50:07 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 #Update and Upgrade System
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
 
 #Install Nala
 echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
 wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg
+
+sudo apt isntall Nala -y
+
+#Config Nala
+sudo nala fetch --auto
