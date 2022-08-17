23933e645d9797e168117aaa42160d09f7891c52
Maxyapper7 <65361371+Maxyapper7@users.noreply.github.com>
2022-08-17 17:15:01 -0600

    First Commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Update and Upgrade System
+sudo apt update
+sudo apt upgrade
+
+#Install Nala
+echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
+wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg
