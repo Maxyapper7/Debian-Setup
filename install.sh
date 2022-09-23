515a01e0985ff9833cadc2415a265154108e21a6
Maxyapper7 <65361371+Maxyapper7@users.noreply.github.com>
2022-09-23 09:47:39 -0600

    Updated install script

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 #Update and Upgrade System
 sudo apt update -y
 sudo apt upgrade -y
 
 #Install & Config Nala
-sudo apt isntall Nala -y
+sudo apt install Nala -y
 sudo nala fetch --auto
 
 sudo nala install 
@@ -17,7 +23,7 @@ sudo nala install gnome-software-plugin-flatpak
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 #Install Programs
-sudo nala install steam discord firefox openoffice gnome-tweaks  -y
-flatpak install flathub com.obsproject.Studio -y
+flatpak install flathub com.obsproject.Studio com.valvesoftware.Steam net.davidotek.pupgui2 com.visualstudio.code com.discordapp.Discord net.lutris.Lutris com.usebottles.bottles us.zoom.Zoom org.onlyoffice.desktopeditors org.videolan.VLC com.github.muriloventuroso.easyssh -y
+flatpak install flathub com.google.Chrome org.mozilla.firefox org.polymc.PolyMC -y
 
 echo "Done With Instalation Please Reboot"
