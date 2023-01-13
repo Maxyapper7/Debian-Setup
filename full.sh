357e557586b724a1b8edae5e0e8446e6fd6b22a3
Maxyapper7 <65361371+Maxyapper7@users.noreply.github.com>
2023-01-13 11:21:42 -0700

    Updated Package Install

--- full.sh
+++ full.sh
@@ -4,18 +4,13 @@ apt update -y
 apt upgrade -y
 
 # Install flatpak
-apt install flatpak -y
-apt install gnome-software-plugin-flatpak -y
+apt install flatpak gnome-software-plugin-flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Install apps
-flatpak install -y flathub org.polymc.PolyMC com.github.muriloventuroso.easyssh net.davidotek.pupgui2 org.onlyoffice.desktopeditors us.zoom.Zoom com.mattjakeman.ExtensionManager com.obsproject.Studio io.github.shiftey.Desktop com.discordapp.Discord
+flatpak install -y flathub org.polymc.PolyMC net.davidotek.pupgui2 org.onlyoffice.desktopeditors com.mattjakeman.ExtensionManager com.obsproject.Studio io.github.shiftey.Desktop com.discordapp.Discord com.valvesoftware.Steam
 
-apt install vlc audacity btop -y
-
-#Steam
-wget https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb
-apt install ./steam.deb -y
+apt install vlc audacity btop gimp -y
 
 #Chrome
 
@@ -24,30 +19,35 @@ apt install ./steam.deb -y
 apt install nala -y
 
 # Config Nala
-echo 'apt() {'  >> .bashrc
-echo   'command nala "$@"' >> .bashrc
-echo '}' >> .bashrc
-echo 'sudo() {' >> .bashrc
-echo   'if [ "$1" = "apt" ]; then' >> .bashrc
-echo     'shift' >> .bashrc
-echo     'command sudo nala "$@"' >> .bashrc
-echo   'else' >> .bashrc
-echo     'command sudo "$@"' >> .bashrc
-echo   'fi' >> .bashrc
-echo '}' >> .bashrc
-echo '' >> .bashrc
+echo 'apt() {'  >> ~/.bashrc
+echo   'command nala "$@"' >> ~/.bashrc
+echo '}' >> ~/.bashrc
+echo 'sudo() {' >> ~/.bashrc
+echo   'if [ "$1" = "apt" ]; then' >> ~/.bashrc
+echo     'shift' >> ~/.bashrc
+echo     'command sudo nala "$@"' >> ~/.bashrc
+echo   'else' >> ~/.bashrc
+echo     'command sudo "$@"' >> ~/.bashrc
+echo   'fi' >> ~/.bashrc
+echo '}' >> ~/.bashrc
+echo '' >> ~/.bashrc
 
-nala fetch --auto
+nala fetch --auto -y
 
 # Install Aliases
 cp .bash_aliases ~/.bash_aliases
 
-echo '' >> .bashrc
-echo 'if [ -f ~/.bash_aliases ]; then' >> .bashrc
-echo . /home/max/.bash_aliases >> .bashrc
-echo fi  >> .bashrc
-echo '' >> .bashrc
+echo '' >> ~/.bashrc
+echo 'if [ -f ~/.bash_aliases ]; then' >> ~/.bashrc
+echo . /home/$USER/.bash_aliases >> ~/.bashrc
+echo fi  >> ~/.bashrc
+echo '' >> ~/.bashrc
 
 # Move Dotfiles
-#git clone https://github.com/Maxyapper7/gnome-dotfiles.git
-
+git clone https://github.com/Maxyapper7/gnome-dotfiles.git
+cp gnome-dotfiles/audacity ~/.config
+cp gnome-dotfiles/GIMP ~/.config
+cp gnome-dotfiles/nautilus ~/.config
+cp gnome-dotfiles/neofetch ~/.config
+cp gnome-dotfiles/variety ~/.config
+cp gnome-dotfiles/btop ~/.config
--- install.sh
+++ install.sh
@@ -19,9 +19,9 @@ read -p 'y, or n: ' gnomeapps
 
 if [ $gnomeapps == 'y' ]
   then
-  apt-get -qq autoclean && apt-get -qq purge -y gnome-2048 gnome-games gnome-sudoku gnome-sushi gnome-chess gnome-contacts gnome-mahjongg gnome-mines gnome-maps gnome-nibbles gnome-online-miners gnome-documents swell-foop shotwell gnome-klotski gnome-robots gnome-taquin hitori five-or-more four-in-a-row aisleriot lightsoff quadrapassel tali gnome-tetravex;
-apt-get -qq remove -y libgnome-games-support-* libmusicbrainz* shotwell-common;
+  apt purge -y gnome-2048 gnome-games gnome-sudoku gnome-sushi gnome-chess gnome-contacts gnome-mahjongg gnome-mines gnome-maps gnome-nibbles gnome-online-miners gnome-documents swell-foop shotwell gnome-klotski gnome-robots gnome-taquin hitori five-or-more four-in-a-row aisleriot lightsoff quadrapassel tali gnome-tetravex
 fi
+
 apt update -y
 apt upgrade -y
 
